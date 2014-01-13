54d5d71d39b23ece7ae45ab31689429b4a170edd
Manish Shankla <mshankla@gmail.com>
2014-01-13 11:44:17 -0800

    Pushing maps application tests and shit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Maps1::Application.config.secret_key_base = 'eb146b34fa9ba66cdafc1a95ccdc8b4da134fd7b319a709732859ebc843633d53f853f8934624f8aff47f23b90524675d04df7691df169173f7a2717910f7299'
